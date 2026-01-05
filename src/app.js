debbd73ff45fbb4b9fea07a7b4ca8a0a465cca23
juanfelipe_1510 <juanfelipe_1510@hotmail.com>
2026-01-05 00:07:27 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+  window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  function comandos(pronoun, adj, noun) {
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++){
+        for (let k = 0; k < noun.length; k++) {
+     console.log(pronoun[i] + adj[j] + noun[k] + ".com")
+        }
+      }
+    }
+  }
+comandos (pronoun,adj,noun)
+}
