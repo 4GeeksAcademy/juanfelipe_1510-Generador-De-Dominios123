17b5782af7868e3ecbb492bce458db160c3fb6cc
juanfelipe_1510 <juanfelipe_1510@hotmail.com>
2026-01-10 00:02:14 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import "./assets/img/4geeks.ico";
   let pronoun = ['the', 'our'];
   let adj = ['great', 'big'];
   let noun = ['jogger', 'racoon'];
-  function comandos(pronoun, adj, noun) {
+ 
     for (let i = 0; i < pronoun.length; i++) {
       for (let j = 0; j < adj.length; j++){
         for (let k = 0; k < noun.length; k++) {
@@ -18,6 +18,5 @@ import "./assets/img/4geeks.ico";
         }
       }
     }
-  }
-comandos (pronoun,adj,noun)
+
 }
